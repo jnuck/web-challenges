c4b1f6237d82d1b14518356b5955b3039d45873d
Joshua <joshuanuckel@icloud.com>
2024-12-04 11:43:39 +0100

    Solve challenge

--- js-basics-and-variables/js-basics-and-variables_social-media-post/index.js
+++ js-basics-and-variables/js-basics-and-variables_social-media-post/index.js
@@ -10,7 +10,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Hello!";
+const content = "My name is Joshua";
+let likes = 100;
+const user = "Joshua";
+let isReported = false;
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +22,15 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+// Log all variables to the console
+console.log(title);
+console.log(content);
+console.log(likes);
+console.log(user);
+console.log(isReported);
+
+// Increase number of likes by 1
+console.log(likes + 1);
 
 // --^-- write your code here --^--
