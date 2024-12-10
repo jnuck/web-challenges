74a3766c9e0b0c70cca08ec035da2ca626999a88
Joshua <joshuanuckel@icloud.com>
2024-12-10 15:46:09 +0100

    Solve challenge

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,13 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const toastElement = document.createElement("li");
+  toastElement.classList.add("toast-container__message");
+  toastContainer.append(toastElement);
+  toastElement.textContent = "Hello";
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
