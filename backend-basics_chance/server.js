8e3845320060dddd87adcb437227b9f6ec422c5c
Joshua Nuckel <joshuanuckel@icloud.com>
2025-01-24 11:27:27 +0100

    Solve chance challenge

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+let name = chance.name();
+let age = chance.age();
+let profession = chance.profession();
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+});
